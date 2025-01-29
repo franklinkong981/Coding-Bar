70b3cef31a52dc98cdf217fc9810a2467978f68b
Franklin Kong <franklinkong981@gmail.com>
2025-01-28 18:58:01 -0800

    Started Part II by writing function to make sure input is square array

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,23 @@
-function unroll(squareArray) {
+function unroll(inputArray) {
+  if (!isSquareArray(squareArray)) return "Invalid input. Your input array must be a square array.";
 
+  let unrolledArray = [];
+
+  return unrolledArray;
+}
+
+//Checks to make sure if input is a square array of arrays, where the input is an array, all its elements are arrays, and every array
+// it contains has a length equal to inputArray.
+function isSquareArray(inputArray) {
+  if (!(Array.isArray(inputArray))) return false;
+
+  //make sure every array in inputArray is an array and the length of each inner array = the length of the outer array.
+  for (let innerArray of inputArray) {
+    if (!(Array.isArray(innerArray))) return false;
+    if (innerArray.length !== inputArray.length) return false;
+  }
+
+  return true;
 }
 
 module.exports = unroll;
