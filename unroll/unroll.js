998fc8242e6fdd6c3a21e6ddb50a45ab1a4dfe14
Franklin Kong <franklinkong981@gmail.com>
2025-01-28 20:56:18 -0800

    Finally figured out solution to unroll problem

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,22 +1,14 @@
 /* Unrolls a square array of arrays into a 1-D array that contains all the individual values in the inputArray in a spiral order. */
 function unroll(inputArray) {
-  if (!isSquareArray(squareArray)) return "Invalid input. Your input array must be a square array.";
+  if (!isSquareArray(inputArray)) return "Invalid input. Your input array must be a square array.";
 
   if (inputArray.length === 0) return [];
+  else if (inputArray.length === 1) return [inputArray[0][0]];
 
   //we know the first value in the unrolledArray will be the first element in the first inner array in inputArray.
-  const unrolledArray = [inputArray[0][0]];
-  const dim = inputArray.length;
-  let currentRow = 0;
-  let currentColumn = 0;
-  while (unrolledArray.length < Math.pow(dim, 2)) {
-    const [nextRow, nextColumn] = getNextRowAndColumn(currentRow, currentColumn, dim);
-    currentRow = nextRow;
-    currentColumn = nextColumn;
-
-    unrolledArray.push(inputArray[currentRow][currentColumn]);
-  }
-
+  const unrolledArray = [];
+  unrolledArray.push(...unrollOuterLayer(inputArray), ...unroll(removeOuterLayer(inputArray)));
+  
   return unrolledArray;
 }
 
@@ -33,13 +25,45 @@ function isSquareArray(inputArray) {
   return true;
 }
 
+function unrollOuterLayer(inputArray) {
+  const unrolledOuterLayer = [];
+  let currentRowIndex = 0;
+  let currentColumnIndex = 0;
+
+  while (unrolledOuterLayer.length < getOuterLayerLength(inputArray.length)) {
+    unrolledOuterLayer.push(inputArray[currentRowIndex][currentColumnIndex]);
+
+    const [nextRowIndex, nextColumnIndex] = getNextRowAndColumn(currentRowIndex, currentColumnIndex, inputArray.length - 1);
+    currentRowIndex = nextRowIndex;
+    currentColumnIndex = nextColumnIndex;
+  }
+
+  return unrolledOuterLayer;
+}
+
+function getOuterLayerLength(arr_length) {
+  return arr_length + (arr_length - 1) + (arr_length - 1) + (arr_length - 2);
+}
+
 /* Given the current row index, column index, and length of the 2-D input array, determine the next row index and column index 
 given that we are going in a clockwise spiral order. */
-function getNextRowAndColumn(row, column, dimension) {
-  if (row <= column && row + column < dimension) return [row, column + 1];
-  else if (row < column && row + column >= dimension) return [row + 1, column];
-  else if (row >= column && row + column > dimension) return [row, column - 1];
-  else if (row > column && row + column <= dimension) return [row - 1, column];
+function getNextRowAndColumn(rowIndex, columnIndex, last_index) {
+  if (rowIndex <= columnIndex && rowIndex + columnIndex < last_index) return [rowIndex, columnIndex + 1];
+  else if (rowIndex < columnIndex && rowIndex + columnIndex >= last_index) return [rowIndex + 1, columnIndex];
+  else if (rowIndex >= columnIndex && rowIndex + columnIndex > last_index) return [rowIndex, columnIndex - 1];
+  else if (rowIndex > columnIndex && rowIndex + columnIndex <= last_index) return [rowIndex - 1, columnIndex];
+}
+
+function removeOuterLayer(inputArray) {
+  inputArray.shift();
+  inputArray.pop();
+
+  for (let innerArray of inputArray) {
+    innerArray.shift();
+    innerArray.pop();
+  }
+
+  return inputArray;
 }
 
 const square = [
@@ -49,6 +73,13 @@ const square = [
 	[13,14,15,16]
 ];
 
-console.log(unroll(square));
+const smallerSquare = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"]
+];
 
-module.exports = {unroll, isSquareArray, getNextRowAndColumn};
+console.log(unroll(square));
+console.log(unroll(smallerSquare));
+
+module.exports = {unroll, isSquareArray, unrollOuterLayer, getOuterLayerLength, getNextRowAndColumn, removeOuterLayer};
