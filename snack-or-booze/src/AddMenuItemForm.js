c8b3c5524a5b3f363c331ec6b0a22f90e649be1f
Franklin Kong <franklinkong981@gmail.com>
2025-02-09 13:52:40 -0800

    Finished JSX of form that adds new menu item

--- snack-or-booze/src/AddMenuItemForm.js
+++ snack-or-booze/src/AddMenuItemForm.js
@@ -41,23 +41,23 @@ const AddMenuItemForm = ({addItemFunc, menuToAddTo}) => {
       size="25" value={formik.values.name} onChange={formik.handleChange}/>
       {formik.errors.name ? <div className="AddMenuItemForm-error">{formik.errors.name}</div> : null} <br/>
 
-      <label htmlFor="AddMenuItemForm-name-field" className="AddMenuItemForm-label">Name:</label>
-      <input id="AddMenuItemForm-name-field" className="AddMenuItemForm-input" type="text" name="name"
-      size="25" value={formik.values.name} onChange={formik.handleChange}/>
+      <label htmlFor="AddMenuItemForm-description-field" className="AddMenuItemForm-label">Description:</label>
+      <input id="AddMenuItemForm-description-field" className="AddMenuItemForm-input" type="text" name="description"
+      size="25" value={formik.values.description} onChange={formik.handleChange}/>
       {formik.errors.name ? <div className="AddMenuItemForm-error">{formik.errors.name}</div> : null} <br/>
 
-      <label htmlFor="AddMenuItemForm-name-field" className="AddMenuItemForm-label">Name:</label>
-      <input id="AddMenuItemForm-name-field" className="AddMenuItemForm-input" type="text" name="name"
-      size="25" value={formik.values.name} onChange={formik.handleChange}/>
+      <label htmlFor="AddMenuItemForm-recipe-field" className="AddMenuItemForm-label">Recipe Instructions:</label>
+      <input id="AddMenuItemForm-recipe-field" className="AddMenuItemForm-input" type="text" name="recipe"
+      size="25" value={formik.values.recipe} onChange={formik.handleChange}/>
       {formik.errors.name ? <div className="AddMenuItemForm-error">{formik.errors.name}</div> : null} <br/>
 
-      <label htmlFor="AddMenuItemForm-name-field" className="AddMenuItemForm-label">Name:</label>
-      <input id="AddMenuItemForm-name-field" className="AddMenuItemForm-input" type="text" name="name"
-      size="25" value={formik.values.name} onChange={formik.handleChange}/>
+      <label htmlFor="AddMenuItemForm-serve-field" className="AddMenuItemForm-label">Serving Instructions:</label>
+      <input id="AddMenuItemForm-serve-field" className="AddMenuItemForm-input" type="text" name="serve"
+      size="25" value={formik.values.serve} onChange={formik.handleChange}/>
       {formik.errors.name ? <div className="AddMenuItemForm-error">{formik.errors.name}</div> : null} <br/>
 
-      <button className="NewColorForm-submit-button" type="submit">Add Color</button>
-      <button className="NewColorForm-home-button" type="button" onClick={() => navigate("/colors")}>Back to Home Page</button>
+      <button className="AddMenuItemForm-submit-button" type="submit">Add {isSnackMenu ? "Snack" : "Drink"}</button>
+      <button className="AddMenuItemForm-back-button" type="button" onClick={() => isSnackMenu ? navigate("/snacks") : navigate("/drinks")}>Back</button>
     </form>
   );
 };
