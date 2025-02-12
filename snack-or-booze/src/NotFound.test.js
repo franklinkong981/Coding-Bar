be9a5c5fa2a733f9a1df36c04cca2eeb0907dd7b
Franklin Kong <franklinkong981@gmail.com>
2025-02-12 12:24:25 -0800

    Tested content of NotFound component

--- snack-or-booze/src/NotFound.test.js
+++ snack-or-booze/src/NotFound.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {render} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 import {MemoryRouter} from "react-router-dom";
 
 import NotFound from "./NotFound";
@@ -19,5 +20,20 @@ describe("Test <NotFound/> rendering", function () {
       </MemoryRouter>
     ));
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("Make sure NotFound component contains the correct content", function() {
+  it("Make sure it contains the not found message and home link", function() {
+    const {getByText} = render((
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>
+    ));
+
+    const notFoundMessage = getByText("Hmmm. I can't seem to find the page you're looking for.");
+    expect(notFoundMessage).toBeInTheDocument();
+    const homeLink = getByText("Home");
+    expect(homeLink).toBeInTheDocument();
   });
 });
